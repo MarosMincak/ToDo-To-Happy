1a1a38c6b6a4d6ef3c45efa072fbb1c0afde3b10
Maroš Minčák <maros.mincak11@gmail.com>
2021-07-12 11:33:10 +0200

    Functionality Udemy, Design Yablko, not completed

---
+++ todo/static/script.js
@@ -0,0 +1,20 @@
+/*window.onload = function() {
+    if (window.jQuery) {
+        alert('jQuery is loaded');
+    } else {
+        alert('jQuery is not loaded');
+    }
+}
+
+
+$('#add-new').append(
+    '<div onclick="addBook('+quote.userId+', \''+quote.book+'\')">'+quote.book+'</div>'
+)
+
+$(document).ready(function () {
+    $('#add-new').on('click', function (){
+        alert('Clicked');
+    });
+});
+
+ */
